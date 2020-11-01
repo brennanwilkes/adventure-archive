f1f02563019a86fba12e531a101afe2d40e18f40
Brennan Wilkes <brennan@codexwilkes.com>
2020-11-01 13:21:35 -0800

    feat(dev): Name and num pages info

--- dev/scraper.sh
+++ dev/scraper.sh
@@ -1,5 +1,20 @@
+#!/bin/bash
+
+filterByRegex(){
+	while read line; do
+		echo "$line" | grep -oE "$1" | sed -E "s/$1/\1/g"
+	done
+}
+
+
 webpage="https://www.lonelyplanet.com/thorntree/forums/"
 indexDate=$( curl -Ls $webpage )
-forumRegex='href="\/thorntree\/forums\/([^>%]+)">'
-forums=$( echo $indexDate | grep -oE "$forumRegex" | sed -E "s/$forumRegex/\1/g" )
-echo $forums
+forums=$( echo $indexDate | filterByRegex 'href="\/thorntree\/forums\/([^>%]+)">' )
+#echo $forums
+
+for page in $forums; do
+	pageData=$( curl -Ls ${webpage}$page )
+	name=$( echo $pageData | filterByRegex '<h1 class="copy--h1">([^>]+)<\/h1>' | sed 's/&amp;/and/g' )
+	numPages=$( echo $pageData | grep -oE 'pagination__link pagination__link--last[^<]*' | head -n1 | rev | cut -d'>' -f1 | rev )
+	echo $name $numPages
+done;
