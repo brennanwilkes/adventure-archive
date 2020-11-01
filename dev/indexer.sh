992e245d3cd10b65fe61a675a439fdfa6c80f333
Brennan Wilkes <brennan@codexwilkes.com>
2020-11-01 13:56:04 -0800

    feat(dev): Indexer script and data output

--- dev/indexer.sh
+++ dev/indexer.sh
@@ -14,7 +14,11 @@ forums=$( echo $indexDate | filterByRegex 'href="\/thorntree\/forums\/([^>%]+)">
 
 for page in $forums; do
 	pageData=$( curl -Ls ${webpage}$page )
-	name=$( echo $pageData | filterByRegex '<h1 class="copy--h1">([^>]+)<\/h1>' | sed 's/&amp;/and/g' )
+	name=$( echo $pageData | filterByRegex '<h1 class="copy--h1">([^>]+)<\/h1>' | sed 's/&amp;/and/g' | tr -d ',' )
 	numPages=$( echo $pageData | grep -oE 'pagination__link pagination__link--last[^<]*' | head -n1 | rev | cut -d'>' -f1 | rev )
-	echo $name $numPages
+	[ ! -z "$name" ] && [ ! -z "$numPages" ] && {
+		echo ${webpage}$page,$name,$numPages
+	}
 done;
+
+exit 0
