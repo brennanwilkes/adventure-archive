b41485970412c51d3eeb1eced5ee8daca5ac36d6
Brennan Wilkes <brennan@codexwilkes.com>
2020-11-01 21:54:48 -0800

    feat(dev): More advanced html thread parsing

--- dev/threadIndex.sh
+++ dev/threadIndex.sh
@@ -15,8 +15,22 @@ countryEx=$( echo $threadData | cut -d',' -f2 | rev | cut -d'/' -f1 | rev )
 forumName=$( echo $threadData | cut -d',' -f4 )
 fullURL=${rootURL}${subforum}/topics/${thread}/compact?
 
+#echo "$fullURL"
+
 pageData=$( curl -Ls $fullURL )
 pageTitle=$( echo $pageData | filterByRegex '<h1 class=\"topic__title copy--h1\">([^<]+)<\/h1>' )
-echo $pageTitle
+
+##Posts
+echo $pageData | tr -d '^' | tr '\n' '^' | grep -oP '(?<=<tr class=\"post\">).*?(?=<\/tr>)' | while IFS= read -r post; do
+	post=$( echo $post | tr '^' '\n' )
+	user=$( echo $post | filterByRegex 'post__info__username\">([^<]+)<\/presenter>' )
+	time=$( echo $post | filterByRegex '<small class=\"timeago\">([^<]+)<\/small>')
+	content=$( echo $post | grep -oP '(?<=<td class=\"post__text\"><p>).*?(?=<\/p><\/td>)' )
+	position=$( echo $post | filterByRegex '<td class=\"post__position\">([^<]*)<\/td>')
+	[ -z "$position" ] && position="0"
+
+	echo $user $time $position
+done;
+
 
 exit 0
