14f1777c16faf1b0a288303b5d4ae219c10e1a41
Brennan Wilkes <brennan@codexwilkes.com>
2020-11-15 22:55:38 -0800

    fix: Mongo bactch update upsert instead of insertMany

--- dev/threadIndex.sh
+++ dev/threadIndex.sh
@@ -15,6 +15,7 @@ rootURL=$( echo $threadData | cut -d',' -f1 )
 subforum=$( echo $threadData | cut -d',' -f2 | cut -d'/' -f1 )
 thread=$( echo $threadData | cut -d',' -f3 | rev | cut -d'/' -f1 | rev )
 
+
 countryEx=$( echo $threadData | cut -d',' -f2 | rev | cut -d'/' -f1 | rev )
 forumName=$( echo $threadData | cut -d',' -f4 )
 fullURL=${rootURL}${subforum}/topics/${thread}/compact?
@@ -24,29 +25,34 @@ fullURL=${rootURL}${subforum}/topics/${thread}/compact?
 pageData=$( curl -Ls $fullURL )
 pageTitle=$( echo $pageData | filterByRegex '<h1 class=\"topic__title copy--h1\">([^<]+)<\/h1>' )
 
-mongoQuery="db.users.insertMany(["
+mongoUserQuery="db.users.bulkWrite(["
 
+#https://www.lonelyplanet.com/thorntree/forums/africa/topics/africa-branch-faq/compact?
 ##Posts
  while IFS= read -r post; do
 	post=$( echo $post | tr '^' '\n' )
 	user=$( echo $post | filterByRegex 'post__info__username\">([^<]+)<\/presenter>' )
+	userId=$( echo "$user" | shasum | sed 's/[ -]*$//g' )
 	time=$( echo $post | filterByRegex '<small class=\"timeago\">([^<]+)<\/small>')
 	content=$( echo $post | grep -oP '(?<=<td class=\"post__text\"><p>).*?(?=<\/p><\/td>)' )
+	contentStripped=$( echo $content | sed 's/<[^>]*>//g' )
+
 	position=$( echo $post | filterByRegex '<td class=\"post__position\">([^<]*)<\/td>')
 	[ -z "$position" ] && position="0"
 
-	mongoQuery="${mongoQuery}{ name:\"$user\" },"
+	#echo "$user"
+	#echo "$time"
+	#echo "$content"
+
+	mongoUserQuery="${mongoQuery}{updateOne:{filter:{_id:0x$userId},update:{name:\"$user\"},upsert: true}},"
 
 	#echo $user $time $position
 done <<<$(echo $pageData | tr -d '^' | tr '\n' '^' | grep -oP '(?<=<tr class=\"post\">).*?(?=<\/tr>)')
 
 
-mongoQuery=$( echo "$mongoQuery" | rev | cut -c2- | rev )
-mongoQuery=${mongoQuery}"])"
-
-execMongo "db.users.ensureIndex('name', {unique: true});"
-execMongo "$mongoQuery"
-
+mongoUserQuery=$( echo "$mongoUserQuery" | rev | cut -c2- | rev )
+mongoUserQuery=${mongoUserQuery}"])"
+execMongo "$mongoUserQuery"
 
 
 exit 0
