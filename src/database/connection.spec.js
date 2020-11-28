b9de41526db9324416f307acb1e2a1e489be4f84
Brennan Wilkes <brennan@codexwilkes.com>
2020-11-28 11:34:30 -0800

    feat(database): Reset db connection test

--- src/database/connection.spec.js
+++ src/database/connection.spec.js
@@ -4,8 +4,8 @@ test("Verifies mongo connection", () => {
 	return connection.then(data => {
 		expect(data.connections[0].name).toBe("adventureArchive");
 	},err => {
-		expect(true).toBe(true);
-		//throw new Error(`Failed to connect to mongoDB: ${err}`);
+		//expect(true).toBe(true);
+		throw new Error(`Failed to connect to mongoDB: ${err}`);
 	});
 });
 
