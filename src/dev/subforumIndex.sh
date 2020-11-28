ce1e2097101476558cc812e964aa39e80e846a32
Brennan Wilkes <brennan@codexwilkes.com>
2020-11-27 16:46:48 -0800

    chore: stash

--- src/dev/subforumIndex.sh
+++ src/dev/subforumIndex.sh
@@ -6,10 +6,16 @@ export rootPage=$( echo $rootData | cut -d',' -f1 )
 export forum=$( echo $rootData | cut -d',' -f2 )
 export forumEscaped=$( echo $forum | sed 's/\//\\\//g' )
 export rootName=$( echo $rootData | cut -d',' -f3 )
-numPages=$( echo $rootData | cut -d',' -f4 )
+export numPages=$( echo $rootData | cut -d',' -f4 )
+export numPagesParts=$(( $numPages / 20 ))
 
 handlePage(){
 	page="$1"
+
+	if (( (( ${page} % ${numPagesParts} )) == 0 ))
+	then
+		echo "Indexing page ${page}/${numPages}" >&2
+	fi
 
 	threads=$( curl -Ls "${rootPage}${forum}?page=${page}" | grep -o 'href="[^"]*"' | grep -v '[#?:]' | sed -n -e "s/^.*href=\"\/thorntree\/forums\/$forumEscaped\///p" | sed 's/"//g' | grep -v '^topics/new$' )
 	for path in $threads; do
